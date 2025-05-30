7e3df4b862eb039120871218c954bc69f56c2f06
Neeta <153780736+NeetaCode@users.noreply.github.com>
2025-05-30 08:25:05 -0600

    Create Roman to Integer.py

---
+++ Roman to Integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map ={
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000  
+        }
+        res=0
+        prev=0
+
+        for char in reversed(s):
+            value = roman_map[char]
+            if value < prev:
+                res -= value
+            else:
+                res += value
+            prev = value
+
+        return res
+
+        
+        return res
+        
